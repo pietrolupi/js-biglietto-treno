3e61e8a80655ae82693d9bf59c24e2738be42ae2
pietrolupi <pietro.lupi92@gmail.com>
2023-09-07 16:18:54 +0200

    [edit]:js

--- JS/script.js
+++ JS/script.js
@@ -2,30 +2,18 @@ const numKm = prompt('quanti km hai intenzione di percorrere?');
 
 const age = parseInt(prompt('quanti anni hai?'));
 
-const ticketPrice = numKm * 0.21;
-
-const ticketPriceDiscount20 = ticketPrice - (ticketPrice / 5);
-
-const ticketPriceDiscount40 = ticketPrice - (ticketPrice / 2.5);
-
-let customPrice; 
-
-
-
+let customPrice = numKm * 0.21; 
 
 
 if (isNaN(numKm) || isNaN(age)){
   customPrice = '! ERRORE ! Si prega di inserire età e km in formato numerico.'
 }else if(age < 18){
-  customPrice = (Math.round(ticketPriceDiscount20 * 100) / 100) + '€';
+  customPrice = (customPrice - (customPrice * 0.2)).toFixed(2) + '€';
 }else if(age > 65){
-  customPrice = (Math.round(ticketPriceDiscount40 * 100) / 100) + '€';
+  customPrice = (customPrice - (customPrice * 0.4)).toFixed(2) + '€';
 }else{
-  customPrice = (Math.round(ticketPrice * 100) / 100) + '€';
+  customPrice = customPrice.toFixed(2) + '€';
 };
-
-
-
 
 
 document.getElementById('output').innerHTML = customPrice;
