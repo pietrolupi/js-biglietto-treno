723c8f0d806aa0747e3151cbf91f185dd04d6291
pietrolupi <pietro.lupi92@gmail.com>
2023-09-07 15:02:18 +0200

    [edit]: js

--- JS/script.js
+++ JS/script.js
@@ -8,6 +8,18 @@ const ticketPriceDiscount20 = ticketPrice - (ticketPrice / 5);
 
 const ticketPriceDiscount40 = ticketPrice - (ticketPrice / 2.5);
 
+let customPrice;
 
-console.log(ticketPrice);
-console.log(ticketPriceDiscount40);
+if(age < 18){
+  customPrice = Math.round(ticketPriceDiscount20 * 100) / 100;
+}else if(age > 65){
+  customPrice = Math.round(ticketPriceDiscount40 * 100) / 100;
+}else{
+  customPrice =  Math.round(ticketPrice * 100) / 100;
+};
+
+
+
+const message = 'Salve gentile cliente, il costo stimato del suo biglietto corrisponde a '+ customPrice + ' €';
+
+document.getElementById('output').innerHTML = message;
