fc918b7184edb202757599a7afcc0067291c87de
pietrolupi <pietro.lupi92@gmail.com>
2023-09-07 14:37:01 +0200

    [edit]:js

--- JS/script.js
+++ JS/script.js
@@ -4,4 +4,10 @@ const age = prompt('quanti anni hai?');
 
 const ticketPrice = numKm * 0.21;
 
+const ticketPriceDiscount20 = ticketPrice - (ticketPrice / 5);
 
+const ticketPriceDiscount40 = ticketPrice - (ticketPrice / 2.5);
+
+
+console.log(ticketPrice);
+console.log(ticketPriceDiscount40);
